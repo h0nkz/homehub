64c2e777209150553ba9c04d7bebbb85495d25ea
h0nkz <102213145+h0nkz@users.noreply.github.com>
2026-01-06 16:44:49 +0100

    Add bottom navigation bar with style

--- homehub-client/src/App.tsx
+++ homehub-client/src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { AppBar, Button, colors, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, BottomNavigation, BottomNavigationAction, Button, colors, IconButton, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import React from 'react';
+import { BorderAllRounded, BorderBottom, BorderColor } from '@mui/icons-material';
 
 
 /*
@@ -18,17 +18,6 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 function App() {
 
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: (theme.vars ?? theme).palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-  }));
-
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
@@ -36,24 +25,54 @@ function App() {
         main: '#000800ff',
       },
       secondary: {
-        main: '#059400ff'
+        main: '#00ee00',
       },
       text: {
         primary: '#00ee00'
       },
     },
     typography: {
-      fontFamily: 'VT323',
+      fontFamily: 'monospace',
       fontWeightBold: 800,
     },
     components: {
-    MuiToolbar: {
-      defaultProps: {
-        color: '#000800ff' // not working
+      MuiToolbar: {
+        defaultProps: {
+          color: '#000800ff' // not working
+        }
       }
     }
-    }
   });
+
+  const HubAppBar = () => (
+    <AppBar position="static" sx={{
+      backgroundColor: darkTheme.palette.primary.main
+    }}>
+      <Toolbar sx={{
+        width: 1,
+        backgroundColor: '#000800ff'
+      }}>
+        <Grid container spacing={1} width={1}>
+          <Grid size={9}>
+            <AppBarText color="secondary">
+              HOMEHUB
+            </AppBarText>
+          </Grid>
+          <Grid size={1}>
+            <AppBarText>
+              {currentTime}
+            </AppBarText>
+          </Grid>
+          <Grid size={1}>
+            <AppBarText>
+              <WbSunnyIcon sx={{ fontSize: 16 }} /> {currentTemp}
+            </AppBarText>
+          </Grid>
+
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  )
 
   const AppBarText = styled(Typography)(({ theme }) => ({
     fontSize: 20,
@@ -62,42 +81,90 @@ function App() {
     fontWeight: theme.typography.fontWeightBold
   }))
 
+
+  interface StyledTabsProps {
+    children?: React.ReactNode;
+    value: string;
+    onChange: (event: React.SyntheticEvent, newValue: string) => void;
+  }
+  const StyledTabs = styled((props: StyledTabsProps) => (
+    <Tabs
+      {...props}
+      centered
+      sx={{
+        color: 'secondary.main'
+      }}
+    />
+  ))({
+    '& .MuiTabs-indicator': {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  });
+
+  interface StyledTabProps {
+    label: string;
+    value: string;
+  }
+
+  const StyledTab = styled((props: StyledTabProps) => (
+    <Tab disableRipple {...props} />
+  ))(({ theme }) => ({
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightBold,
+    fontSize: theme.typography.pxToRem(18),
+    marginRight: theme.spacing(5),
+    color: '#00ee00',
+    border: 2,
+    '&.Mui-selected': {
+      backgroundColor: theme.palette.secondary.main,
+      border: 2,
+      borderRadius: 3
+
+    },
+  }));
+
   const currentTime = '13:37'
   const currentTemp = '17°C'
+
+  const [tabValue, setTabValue] = React.useState('main');
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: string) => {
+    setTabValue(newValue);
+  };
 
   return (
     <div id="grandparent">
       <ThemeProvider theme={darkTheme}>
-        <AppBar position="static" sx={{
-          backgroundColor: darkTheme.palette.primary.main
-        }}>
-          <Toolbar sx={{
-            width: 1,
-            backgroundColor: '#000800ff'
+        <HubAppBar />
+        <Box>
+          <Box sx={{
+            border: 2,
+            height: 450,
+            width: 972,
+            marginLeft: 3,
+            marginRight: 3,
+            borderColor: 'secondary.main'
           }}>
-            <Grid container spacing={1} width={1}>
-              <Grid size={9}>
-                <AppBarText color="secondary">
-                  HOMEHUB
-                </AppBarText>
-              </Grid>
-              <Grid size={1}>
-                <AppBarText>
-                  {currentTime}
-                </AppBarText>
-              </Grid>
-              <Grid size={1}>
-                <AppBarText>
-                  <WbSunnyIcon sx={{ fontSize: 16 }} /> {currentTemp}
-                </AppBarText>
-              </Grid>
+            {tabValue}
+            <Grid container>
 
             </Grid>
-          </Toolbar>
-        </AppBar>
-        <Grid container spacing={2}>
-          
-        </Grid>
+          </Box>
+          <StyledTabs
+            value={tabValue}
+            onChange={handleChangeTab}>
+            <StyledTab value="main" label="MAIN"/>
+            <StyledTab value="todo" label="TODO" />
+            <StyledTab value="errands" label="ERRANDS" />
+            <StyledTab value="calendar" label="CALENDAR" />
+          </StyledTabs>
+        </Box>
 
       </ThemeProvider>
     </div>
